c842c3dfd22cb9b61d280a518b41314b02abe723
KirillTotkov <kirillatot@gmail.com>
2024-11-03 18:36:07 +0300

    Добавить подтверждение по почте

--- DelphicGames/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ DelphicGames/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -21,10 +21,7 @@ namespace DelphicGames.Areas.Identity.Pages.Account
             _userManager = userManager;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+
         [TempData]
         public string StatusMessage { get; set; }
 
@@ -43,7 +40,7 @@ namespace DelphicGames.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            StatusMessage = result.Succeeded ? "Спасибо за подтверждение вашего email." : "Ошибка подтверждения email.";
             return Page();
         }
     }
