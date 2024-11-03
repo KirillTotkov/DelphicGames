5d6d060b82d538cca30a3df21d57e3754fbe9d0b
KirillTotkov <kirillatot@gmail.com>
2024-11-03 12:58:24 +0300

    Добавить страницу для создания пользователей

---
+++ DelphicGames/Pages/Admin/Users/Create.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using DelphicGames.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DelphicGames.Pages.Admin.Users;
+
+[Authorize(Roles = "Root")]
+public class CreateModel : PageModel
+{
+    private readonly UserManager<User> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly ILogger<CreateModel> _logger;
+
+    public CreateModel(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, ILogger<CreateModel> logger)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _logger = logger;
+    }
+
+    [BindProperty]
+    public CreateUserInput Input { get; set; }
+    public List<SelectListItem> Roles { get; set; }
+
+
+    public async Task OnGetAsync()
+    {
+        Roles = _roleManager.Roles.Select(r => new SelectListItem { Value = r.Name, Text = r.Name }).ToList();
+    }
+
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            Roles = _roleManager.Roles.Select(r => new SelectListItem { Value = r.Name, Text = r.Name }).ToList();
+            return Page();
+        }
+
+        var user = new User { UserName = Input.Email, Email = Input.Email };
+        var result = await _userManager.CreateAsync(user, Input.Password);
+
+        if (result.Succeeded)
+        {
+            if (!string.IsNullOrEmpty(Input.Role))
+            {
+                await _userManager.AddToRoleAsync(user, Input.Role);
+            }
+            _logger.LogInformation("Пользователь создан и назначена роль.");
+            return RedirectToPage("Index");
+        }
+
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        Roles = _roleManager.Roles.Select(r => new SelectListItem { Value = r.Name, Text = r.Name }).ToList();
+        return Page();
+    }
+
+    public class CreateUserInput
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
