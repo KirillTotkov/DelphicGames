584540c58ceadf37318e4d092a82b8f2b7ce4768
KirillTotkov <kirillatot@gmail.com>
2024-11-03 18:33:14 +0300

    Добавить реализацию отправки писем

--- DelphicGames/Program.cs
+++ DelphicGames/Program.cs
@@ -1,11 +1,15 @@
 using DelphicGames.Data;
 using DelphicGames.Data.Models;
+using DelphicGames.Models;
 using DelphicGames.Services;
 using DelphicGames.Services.Streaming;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -13,12 +17,29 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
 builder.Services.AddRazorPages();
 
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
         .UseSnakeCaseNamingConvention());
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationContext>();
+builder.Services.AddDefaultIdentity<User>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequiredLength = 8;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationContext>()
+    .AddDefaultTokenProviders();
+
 
 builder.Services.AddSingleton<StreamManager>();
 builder.Services.AddScoped<CameraService>();
---
+++ DelphicGames/Services/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using DelphicGames.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+
+namespace DelphicGames.Services;
+
+public class EmailSender : IEmailSender
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailSender(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        Console.WriteLine(_emailSettings.Password);
+        
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Дельфийкие игры", _emailSettings.From));
+        message.To.Add(new MailboxAddress("", email));
+        message.Subject = subject;
+
+        var bodyBuilder = new BodyBuilder
+        {
+            HtmlBody = htmlMessage
+        };
+        message.Body = bodyBuilder.ToMessageBody();
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_emailSettings.Username, _emailSettings.Password);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
