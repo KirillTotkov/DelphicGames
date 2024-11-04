cdcfcd84abaab156d0ebdcaca00bada2834e4451
KirillTotkov <kirillatot@gmail.com>
2024-11-04 11:09:23 +0300

    Изменить CameraService для возврата DTO при добавлении и добавить проверку уникальности по URL; добавить метод для привязки платформы к камере

--- DelphicGames/Services/CameraService.cs
+++ DelphicGames/Services/CameraService.cs
@@ -15,11 +15,16 @@ public class CameraService
         _logger = logger;
     }
 
-    public async Task<Camera> CreateCamera(AddCameraDto dto)
+    public async Task<GetCameraDto> CreateCamera(AddCameraDto dto)
     {
         if (await _context.Cameras.AnyAsync(c => c.Name == dto.Name))
         {
             throw new InvalidOperationException($"Камера с именем {dto.Name} уже существует.");
+        }
+
+        if (await _context.Cameras.AnyAsync(c => c.Url == dto.Url))
+        {
+            throw new InvalidOperationException($"Камера с URL {dto.Url} уже существует.");
         }
 
         var camera = new Camera
@@ -30,13 +35,8 @@ public class CameraService
 
         _context.Cameras.Add(camera);
         await _context.SaveChangesAsync();
-        return camera;
-    }
 
-    public async Task CreateCameras(List<Camera> cameras)
-    {
-        _context.Cameras.AddRange(cameras);
-        await _context.SaveChangesAsync();
+        return new GetCameraDto(camera.Id, camera.Name, camera.Url);
     }
 
     public async Task<GetCameraDto?> GetCamera(int id)
@@ -76,6 +76,13 @@ public class CameraService
             return null;
         }
 
+        if (await _context.Cameras.AnyAsync(c => c.Name == camera.Name && c.Id != id))
+        {
+            throw new InvalidOperationException($"Камера с именем {camera.Name} уже существует.");
+        }
+
+
+
         existingCamera.Name = camera.Name;
         existingCamera.Url = camera.Url;
 
@@ -94,6 +101,31 @@ public class CameraService
         _context.Cameras.Remove(camera);
         await _context.SaveChangesAsync();
         return true;
+    }
+
+    // Добавление платформы к камере 
+    public async Task AddPlatformToCamera(int cameraId, int platformId)
+    {
+        var camera = await _context.Cameras.FindAsync(cameraId);
+        if (camera == null)
+        {
+            throw new InvalidOperationException($"Камера с ID {cameraId} не найдена.");
+        }
+
+        var platform = await _context.Platforms.FindAsync(platformId);
+        if (platform == null)
+        {
+            throw new InvalidOperationException($"Платформа с ID {platformId} не найдена.");
+        }
+
+        var cameraPlatform = new CameraPlatforms
+        {
+            Camera = camera,
+            Platform = platform
+        };
+
+        _context.CameraPlatforms.Add(cameraPlatform);
+        await _context.SaveChangesAsync();
     }
 
     public async Task<bool> CameraExists(int id)
