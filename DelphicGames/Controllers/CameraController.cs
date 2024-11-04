175cb8665eec1cddfc987325eb1931222fd764ed
KirillTotkov <kirillatot@gmail.com>
2024-11-04 10:34:31 +0300

    Добавить Api CameraController с методами для добавления и получения камер

--- DelphicGames/Controllers/CameraController.cs
+++ DelphicGames/Controllers/CameraController.cs
@@ -1,14 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DelphicGames.Data;
+using DelphicGames.Data.Models;
+using DelphicGames.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DelphicGames.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
-public class CameraController: ControllerBase
+[Route("api/cameras")]
+public class CameraController : ControllerBase
 {
-    [HttpGet]
-    public async Task<IActionResult> Get()
+    private readonly CameraService _cameraService;
+
+    public CameraController(CameraService cameraService)
     {
-        return Ok(new { Message = "Hello, World!" });
+        _cameraService = cameraService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCamera([FromBody] AddCameraDto dto)
+    {
+        await _cameraService.CreateCamera(dto);
+
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCamera(int id)
+    {
+        var camera = await _cameraService.GetCamera(id);
+        if (camera == null)
+        {
+            return NotFound();
+        }
+        return Ok(camera);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCameras()
+    {
+        var cameras = await _cameraService.GetCameras();
+        return Ok(cameras);
     }
 }
+
