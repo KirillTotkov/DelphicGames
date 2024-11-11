a947c1552b1e7705a70796af0d66728988718163
ktotkov <kirillatot@gmail.com>
2024-11-11 13:35:07 +0300

    Добавить пользователя к камере

---
+++ DelphicGames/Data/Migrations/20241111103338_add-user-in-camera.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DelphicGames.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduserincamera : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "user_id",
+                table: "cameras",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_cameras_user_id",
+                table: "cameras",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_cameras_users_user_id",
+                table: "cameras",
+                column: "user_id",
+                principalTable: "AspNetUsers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_cameras_users_user_id",
+                table: "cameras");
+
+            migrationBuilder.DropIndex(
+                name: "ix_cameras_user_id",
+                table: "cameras");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "cameras");
+        }
+    }
+}
--- DelphicGames/Data/Models/User.cs
+++ DelphicGames/Data/Models/User.cs
@@ -6,4 +6,6 @@ public class User : IdentityUser
 {
     public City? City { get; set; }
     public Region? Region { get; set; }
+
+    public List<Camera>? Cameras { get; set; } = new();
 }
