41662d209ac7e7b137b51a701b040cb754f59e88
ktotkov <kirillatot@gmail.com>
2024-11-11 18:19:06 +0300

    Добавить поле StreamUrl в модель Nomination и миграцию для колонки

---
+++ DelphicGames/Data/Migrations/20241111151829_add-stream_url-to-nomination.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DelphicGames.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addstream_urltonomination : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "url",
+                table: "nominations",
+                newName: "stream_url");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "stream_url",
+                table: "nominations",
+                newName: "url");
+        }
+    }
+}
--- DelphicGames/Data/Models/Nomination.cs
+++ DelphicGames/Data/Models/Nomination.cs
@@ -4,5 +4,6 @@ public class Nomination
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public string StreamUrl { get; set; }
     public List<Camera> Cameras { get; set; } = [];
 }
