776c8a453dfded9cd1162047f2eb3972a316831b
ktotkov <kirillatot@gmail.com>
2024-11-17 13:23:26 +0300

    Удалить Platform, Переименовать и изменить модель NominationPlatform на StreamEntity

--- DelphicGames/Data/Models/StreamEntity.cs
+++ DelphicGames/Data/Models/StreamEntity.cs
@@ -1,11 +1,11 @@
 ﻿namespace DelphicGames.Data.Models;
 
-public class NominationPlatform
+public class StreamEntity
 {
     public int NominationId { get; set; }
     public Nomination Nomination { get; set; }
-    public int PlatformId { get; set; }
-    public Platform Platform { get; set; }
+    public string PlatformName { get; set; }
+    public string PlatformUrl { get; set; }
     public string? Token { get; set; }
     public int Day { get; set; }
     public bool IsActive { get; set; }
