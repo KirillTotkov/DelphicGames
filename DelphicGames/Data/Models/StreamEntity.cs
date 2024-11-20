839022bf612134bd3ce646e79cb1ac3a9ab947b2
ktotkov <kirillatot@gmail.com>
2024-11-20 20:34:59 +0300

    Обновить модель StreamEntity для поддержки nullable значений и изменить миграции базы данных

--- DelphicGames/Data/Models/StreamEntity.cs
+++ DelphicGames/Data/Models/StreamEntity.cs
@@ -5,10 +5,10 @@ public class StreamEntity
     public int Id { get; set; }
     public int NominationId { get; set; }
     public Nomination Nomination { get; set; }
-    public string PlatformName { get; set; }
-    public string PlatformUrl { get; set; }
+    public string? PlatformName { get; set; }
+    public string? PlatformUrl { get; set; }
     public string? Token { get; set; }
-    public string StreamUrl { get; set; }
+    public string? StreamUrl { get; set; }
     public int Day { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } = false;
 }
