b98a53e6570f9a532ddcd55eef1c49cb84f77d1b
ktotkov <kirillatot@gmail.com>
2024-11-21 09:41:04 +0300

    Добавить уникальные индексы для полей Name и Url в таблицах Cameras и Nominations

--- DelphicGames/Data/Migrations/20241121063651_add-unuque-camera-values.Designer.cs
+++ DelphicGames/Data/Migrations/20241121063651_add-unuque-camera-values.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DelphicGames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DelphicGames.Data.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20241121063651_add-unuque-camera-values")]
+    partial class addunuquecameravalues
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -86,10 +89,6 @@ namespace DelphicGames.Data.Migrations
 
                     b.HasKey("Id")
                         .HasName("pk_nominations");
-
-                    b.HasIndex("Name")
-                        .IsUnique()
-                        .HasDatabaseName("ix_nominations_name");
 
                     b.ToTable("nominations", (string)null);
                 });
---
+++ DelphicGames/Data/Migrations/20241121063651_add-unuque-camera-values.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DelphicGames.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addunuquecameravalues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_cameras_name",
+                table: "cameras",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_cameras_url",
+                table: "cameras",
+                column: "url",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_cameras_name",
+                table: "cameras");
+
+            migrationBuilder.DropIndex(
+                name: "ix_cameras_url",
+                table: "cameras");
+        }
+    }
+}
